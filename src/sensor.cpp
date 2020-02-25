e96aa8aacb71d7f339a47f7dc9b61b9de7e58df8
formatkm <formatkm@msn.com>
2020-02-25 16:16:16 +0800

    测量外部电池电压

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -4,6 +4,7 @@
 #include "config.h"
 
 Adafruit_BMP085 bmp;
+
 void Sensor::getBMP()
 {
 
@@ -29,10 +30,10 @@ int16_t Sensor::getLight()
     return sensorValue;
 }
 
-int16_t Sensor::getBattery()
+float Sensor::getBattery()
 {
 
-    unsigned int sensorValue = analogRead(A1);
+    float sensorValue = Read_Volts();
     //_PL("battery:" + String(sensorValue));
     return sensorValue;
 }
@@ -48,4 +49,33 @@ float Sensor::getTemp()
 float Sensor::getAltitude()
 {
     return altitude;
+}
+/**************
+ * 以外部电压为基准测量1.1v，反过来计算出外部电压 
+ **/
+float Sensor::Read_Volts(void)
+{
+    // Read 1.1V reference against AVcc
+    // set the reference to Vcc and the measurement to the internal 1.1V reference
+    if (ADMUX != ADMUX_VCCWRT1V1)
+    {
+        ADMUX = ADMUX_VCCWRT1V1;
+
+        // Bandgap reference start-up time: max 70us
+        // Wait for Vref to settle.
+        delayMicroseconds(350);
+    }
+
+    // Start conversion and wait for it to finish.
+    ADCSRA |= _BV(ADSC);
+    while (bit_is_set(ADCSRA, ADSC))
+    {
+    };
+
+    // Result is now stored in ADC.
+
+    // Calculate Vcc (in V)
+    float vcc = 1.1 * 1024.0 / ADC;
+
+    return vcc;
 }
--- src/sensor.h
+++ src/sensor.h
@@ -1,12 +1,12 @@
 #ifndef _SENSOR_H_
 #define _SENSOR_H_
-
+#include "Arduino.h"
 class Sensor
 {
 public:
     void getBMP();
     int16_t getLight();
-    int16_t getBattery();
+    float getBattery();
     int32_t getPressure();
     float getTemp();
     float getAltitude();
@@ -15,6 +15,18 @@ private:
     int32_t pressure;
     float temp;
     float altitude;
+
+    float Read_Volts(void);
 };
+
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#define ADMUX_VCCWRT1V1 (_BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1))
+#elif defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+#define ADMUX_VCCWRT1V1 (_BV(MUX5) | _BV(MUX0))
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+#define ADMUX_VCCWRT1V1 (_BV(MUX3) | _BV(MUX2))
+#else
+#define ADMUX_VCCWRT1V1 (_BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1))
+#endif
 
 #endif
